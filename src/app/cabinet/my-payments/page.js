d16a9eaff38a7acfc5283c4338b5fe6c5ece2e81
Ievgen Nechaiev <e.v.nechaev@gmail.com>
2025-06-30 14:22:44 +0000

    update

---
+++ src/app/cabinet/my-payments/page.js
@@ -0,0 +1,35 @@
+"use client";
+
+import React, {useEffect} from "react";
+import {redirect, useRouter} from "next/navigation";
+import MainLayout from "../../../../components/MainLayout";
+import Form404 from "../../../../components/pages/cabinet-page/Form404";
+import useAuthStore from "../../../../components/storage";
+
+
+// export const metadata = {
+//     title: 'Login',
+//     description: '',
+// };
+
+export default function MyPaymentsPage() {
+    const {token, user, isHydrated} = useAuthStore();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!isHydrated) {
+            // Ждем, пока Zustand восстановит состояние
+            return;
+        }
+        if (!token) {
+            router.push('/login');
+        }
+
+    }, [token, router, isHydrated]);
+
+    return (
+        <MainLayout isAuth>
+            <Form404/>
+        </MainLayout>
+    );
+}
