7699246675065cc56173c6dfe7f239a8a096d10a
Ievgen Nechaiev <e.v.nechaev@gmail.com>
2025-07-10 19:38:40 +0000

    contact page added

--- src/app/cabinet/contacts/page.js
+++ src/app/cabinet/contacts/page.js
@@ -81,7 +81,7 @@ export default function MyHomePage() {
                     <div className="row">
                         <div className="col-md-12 col-xl-6 position-relative text-center pointer">
                             <div className="contacts-links-section__card align-content-center">
-                                <a href="https://cms.gov" target="_blank" className="fake-link-block"></a>
+                                <a href="https://www.cms.gov/medicare/medicaid-coordination/center-program-integrity/reporting-fraud" target="_blank" className="fake-link-block"></a>
                                 <h4 style={{fontSize:'24px', fontWeight:'800'}}> Medicare Fraud, Waste & Abuse OIG</h4>
                                 <span style={{fontSize:'22px', fontWeight:'400'}}>Hotline: (800) 447-8477 | CMS.gov</span>
                             </div>
