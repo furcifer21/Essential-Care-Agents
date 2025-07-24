fadbb88d09b8f293fc403e9efaf972919cb7605d
Ievgen Nechaiev <e.v.nechaev@gmail.com>
2025-07-24 10:25:01 +0000

    move forms to my-payments

--- src/app/cabinet/my-data/page.js
+++ src/app/cabinet/my-data/page.js
@@ -52,10 +52,6 @@ export default function MyDataPage() {
         );
     }, [token, router, isHydrated]);
 
-    useEffect(() => {
-        console.log('user updated', user);
-    },[user])
-
     return (
         <MainLayout isAuth>
             <section className="outer-section-margin">
@@ -134,45 +130,6 @@ export default function MyDataPage() {
                                     </div>
                                 </div>
                             </div>
-                            <div className="row mb-4">
-                                <div className="col-md-3 mb-4 mb-md-0">
-                                    <div className="mydata-card bg-transparent d-flex flex-column align-items-start justify-content-start">
-                                        <div className={"mydata-card__title d-flex w-100 align-items-center justify-content-between"}>
-                                            W-9 Form:
-                                            {user?.pdf_w9_path && <span className={'text-orange'}>Filled</span> }
-                                        </div>
-                                        <div className={"mydata-card__text d-flex align-items-center justify-content-between"}>
-                                              <Link
-                                                href={'https://na3.documents.adobe.com/public/esignWidget?wid=CBFCIBAA3AAABLblqZhDgaYtEMcec7O-DMDq0WYBxNlzSVpUAS9kS3Q2Zh8TJPOX4xjzaz9KrQaBeNTDXWqM*&hosted=false'}
-                                                target={'_blank'}
-                                              >{user?.pdf_w9_path
-                                                ? 'Update and Sign'
-                                                : 'Fulfill and Sign'
-                                              }
-                                              </Link>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-md-3 mb-4 mb-md-0">
-                                    <div className="mydata-card bg-transparent d-flex flex-column align-items-start justify-content-start">
-                                        <div className={"mydata-card__title d-flex w-100 align-items-center justify-content-between"}>
-                                            Direct deposit Form:
-                                            {user?.pdf_direct_deposit_path && <span className={'text-orange'}>Filled</span> }
-                                        </div>
-                                        <div className={"mydata-card__text"}>
-                                            <Link
-                                              href={'https://na3.documents.adobe.com/public/esignWidget?wid=CBFCIBAA3AAABLblqZhAs-3pCc-jXJvtDIZw-lClbItdzeFEUpIyTHV5gar3gJhh45jaPB0FcvDGMfiQMg5Q*&hosted=false'}
-                                              target={'_blank'}
-                                            >{user?.pdf_direct_deposit_path
-                                              ? 'Update and Sign'
-                                              : 'Fulfill and Sign'
-                                            }
-                                            </Link>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
                         </div>
                     </div>
                 </div>
--- src/app/cabinet/my-payments/page.js
+++ src/app/cabinet/my-payments/page.js
@@ -1,16 +1,11 @@
 "use client";
 
 import React, {useEffect} from "react";
-import {redirect, useRouter} from "next/navigation";
+import {useRouter} from "next/navigation";
 import MainLayout from "../../../../components/MainLayout";
-import Form404 from "../../../../components/pages/cabinet-page/Form404";
 import { useAuthStore } from "../../../../components/storage";
+import Link from "next/link";
 
-
-// export const metadata = {
-//     title: 'Login',
-//     description: '',
-// };
 
 export default function MyPaymentsPage() {
     const {token, user, isHydrated} = useAuthStore();
@@ -29,7 +24,52 @@ export default function MyPaymentsPage() {
 
     return (
         <MainLayout isAuth>
-            <Form404/>
+            <section className="outer-section-margin">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="row mb-4">
+                                <div className="col-md-3 mb-4 mb-md-0">
+                                    <div className="mydata-card bg-transparent d-flex flex-column align-items-start justify-content-start">
+                                        <div className={"mydata-card__title d-flex w-100 align-items-center justify-content-between"}>
+                                            W-9 Form:
+                                            {user?.pdf_w9_path && <span className={'text-orange'}>Filled</span> }
+                                        </div>
+                                        <div className={"mydata-card__text d-flex align-items-center justify-content-between"}>
+                                            <Link
+                                              href={'https://na3.documents.adobe.com/public/esignWidget?wid=CBFCIBAA3AAABLblqZhDgaYtEMcec7O-DMDq0WYBxNlzSVpUAS9kS3Q2Zh8TJPOX4xjzaz9KrQaBeNTDXWqM*&hosted=false'}
+                                              target={'_blank'}
+                                            >{user?.pdf_w9_path
+                                              ? 'Update and Sign'
+                                              : 'Fulfill and Sign'
+                                            }
+                                            </Link>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-3 mb-4 mb-md-0">
+                                    <div className="mydata-card bg-transparent d-flex flex-column align-items-start justify-content-start">
+                                        <div className={"mydata-card__title d-flex w-100 align-items-center justify-content-between"}>
+                                            Direct deposit Form:
+                                            {user?.pdf_direct_deposit_path && <span className={'text-orange'}>Filled</span> }
+                                        </div>
+                                        <div className={"mydata-card__text"}>
+                                            <Link
+                                              href={'https://na3.documents.adobe.com/public/esignWidget?wid=CBFCIBAA3AAABLblqZhAs-3pCc-jXJvtDIZw-lClbItdzeFEUpIyTHV5gar3gJhh45jaPB0FcvDGMfiQMg5Q*&hosted=false'}
+                                              target={'_blank'}
+                                            >{user?.pdf_direct_deposit_path
+                                              ? 'Update and Sign'
+                                              : 'Fulfill and Sign'
+                                            }
+                                            </Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </MainLayout>
     );
 }
