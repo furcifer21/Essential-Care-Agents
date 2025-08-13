5a44df59b007b983bd4fa78868cb61d330be0ac8
Ievgen Nechaiev <e.v.nechaev@gmail.com>
2025-08-13 12:04:04 +0000

    change important documents in portal

--- components/constants.js
+++ components/constants.js
@@ -3,6 +3,7 @@ export const IMAGE_API_URL = process.env.NEXT_PUBLIC_IMAGE_API_URL;
 export const AGREEMENTS_PRODUCER_PDF_API_URL = process.env.NEXT_PUBLIC_IMAGE_API_URL + '/storage/pdf/producer/';
 export const AGREEMENTS_FEE_PDF_API_URL = process.env.NEXT_PUBLIC_IMAGE_API_URL + '/storage/pdf/fee/';
 export const AVATAR_API_URL = process.env.NEXT_PUBLIC_IMAGE_API_URL + '/storage/avatars/';
+export const IMPORTANT_DOCUMENTS_API_URL = process.env.NEXT_PUBLIC_IMAGE_API_URL + '/storage/important-documents/';
 export const CLIENT_API_URL = process.env.NEXT_PUBLIC_CLIENT_API_URL;
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 export const RECAPTCHA_KEY = process.env.NEXT_PUBLIC_RECAPTCHA_KEY;
